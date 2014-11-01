489a01a56f5afda97ecda385be7afc9934c5d9d9
Ted Jenkins <edwardtjenkins@gmail.com>
2014-11-01 12:34:39 -0600

    fixed router

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,13 +4,10 @@
 
 'use strict';
 
-var thingsRouter = require('../api/things'),
-    otherThingsRouter = require('../api/other-things');
-
 module.exports = function(app) {
 
   // Insert routes below
-  app.use('/api/things', thingsRouter);
-  app.use('/api/other-things', otherThingsRouter);
+  app.use('/api/things', require('../api/things'));
+  app.use('/api/other-things', require('../api/other-things'));
 
 };
