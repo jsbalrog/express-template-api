e60bd76dbedd0de91d5f21120a858ba0a5508217
jsbalrog <edwardtjenkins@gmail.com>
2017-08-14 10:55:42 -0600

    feature: implement create, update and delete

--- server/api/thing/thing.controller.js
+++ server/api/thing/thing.controller.js
@@ -10,6 +10,7 @@
 'use strict';
 
 var Thing = require('./thing.model').Thing;
+var _ = require('lodash');
 
 function handleError(res, err) {
   return res.send(500, err);
@@ -30,4 +31,38 @@ exports.show = function(req, res) {
     if(!thing) { res.send(404); }
     return res.json(thing);
   });
-}
+};
+
+// Create a new thing
+exports.create = function(req, res) {
+  Thing.create(req.body, function(err, thing) {
+    if(err) { return handleError(res, err); }
+    return res.json(201, thing);
+  });
+};
+
+// Update an existing thing
+exports.update = function(req, res) {
+  if(req.body._id) { delete req.body._id; }
+  Thing.findById(req.params.id, function(err, thing) {
+    if(err) { return handleError(res, err) ; }
+    if(!thing) { return res.send(404); }
+    var updated = _.merge(thing, req.body);
+    updated.save(function(err) {
+      if(err) { return handleError(res, err); }
+      return res.json(200, thing);
+    });
+  });
+};
+
+// Delete an existing thing
+exports.destroy = function(req, res) {
+  Thing.findById(req, params.id, function(err, thing) {
+    if(err) { return handleError(res, err); }
+    if(!thing) { return res.send(404); }
+    thing.remove(function(err) {
+      if(err) { return handleError(res, err); }
+      return res.send(204);
+    });
+  });
+};
